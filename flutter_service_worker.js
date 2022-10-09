aaf36d22902cd82ca1d18d45da13c640c6d2d487
beertap <87916129+beertap@users.noreply.github.com>
2022-10-10 00:24:53 +0200

    v0.0.26

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,9 +40,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "3b563806524fd43b1107b750c3c102b6",
 "icons/Icon-maskable-192.png": "3b563806524fd43b1107b750c3c102b6",
 "icons/Icon-maskable-512.png": "3b563806524fd43b1107b750c3c102b6",
-"index.html": "90320edc5b80b4955ee5d32cbac9e4c2",
-"/": "90320edc5b80b4955ee5d32cbac9e4c2",
-"main.dart.js": "6b4ad2eaa66882d3349f23b4dbd03504",
+"index.html": "79e1b48b56e3dd618e01cba05b22548a",
+"/": "79e1b48b56e3dd618e01cba05b22548a",
+"main.dart.js": "6bbf835f3852ffe297a8443fd18f29c1",
 "manifest.json": "a052fc08a99c772d81e2370b55397ace",
 "script.js": "8229bb66964edef18ac2c3aa76b43147",
 "version.json": "1c3ba6d973a11603a84deac549304799"
